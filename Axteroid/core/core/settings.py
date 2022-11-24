8894c8056c9b2826bf70c37554fb3f4b8c9f4a75
mazajo90 <114250491+mazajo90@users.noreply.github.com>
2022-11-23 21:45:15 -0300

    Update settings.py

--- Axteroid/core/core/settings.py
+++ Axteroid/core/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lka%$7n_dsq=1tdet!uwh(@^%m0vj#ro2gp*ahk1t&hk*e5zbh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
